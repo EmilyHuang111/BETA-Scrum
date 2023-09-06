b96985b6392db5bbb68d6bb51f53df9c8a094dc1
AnaySharma01 <115898324+AnaySharma01@users.noreply.github.com>
2023-09-06 17:56:56 -0400

    Update BodyfatProject.py

--- BodyfatProject.py
+++ BodyfatProject.py
@@ -1,1 +1,12 @@
-print("hi")
+def menu():
+  inp = input("1 - Male\n 2 - Female\n 3 - Exit\n")
+  if inp == "1":
+    print("male")
+  elif inp == "2":
+    print("female")
+  elif inp == "3":
+    exit
+  else: 
+    print("Please enter a valid number")
+    menu()
+menu()
