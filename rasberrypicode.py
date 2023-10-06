62269f435ca2099a97671c735d1405dad6695c6f
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-10-05 20:36:42 -0400

    Update rasberrypicode.py

--- rasberrypicode.py
+++ rasberrypicode.py
@@ -1,3 +1,22 @@
-from gpiozero import Robot
-robot = Robot((4, 14), (17, 27))
-motor1.forward()
+#Importing Libraries Needed
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Defining Motors
+GPIO.setmode(GPIO.BOARD)
+left_front_motor = 15
+right_front_motor = 15
+left_back_motor = 15
+right_back_motor = 15
+
+#Setting Up the Motors
+GPIO.setup(left_front_motor,GPIO.OUT)
+GPIO.setup(right_front_motor,GPIO.OUT)
+GPIO.setup(left_back_motor,GPIO.OUT)
+GPIO.setup(right_back_motor,GPIO.OUT)
+
+GPIO.output(left_front_motor, GPIO.HIGH)
+GPIO.output(right_front_motor, GPIO.HIGH)
+GPIO.output(left_back_motor, GPIO.HIGH)
+GPIO.output(right_back_motor, GPIO.HIGH)
+GPIO.cleanup()
