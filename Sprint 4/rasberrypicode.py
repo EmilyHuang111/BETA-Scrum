7e8ff18e976ba4aef9773766608b239848aad46c
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-10-24 21:19:47 -0400

    Add files via upload

---
+++ Sprint 4/rasberrypicode.py
@@ -0,0 +1,26 @@
+#Importing Libraries Needed
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Defining Motors
+GPIO.setmode(GPIO.BOARD)
+left_front_motor = 15
+right_front_motor = 15
+left_back_motor = 15
+right_back_motor = 15
+
+#Setting Up the Motors
+GPIO.setup(left_front_motor,GPIO.OUT)
+GPIO.setup(right_front_motor,GPIO.OUT)
+GPIO.setup(left_back_motor,GPIO.OUT)
+GPIO.setup(right_back_motor,GPIO.OUT)
+
+#Running Motors
+GPIO.output(left_front_motor, GPIO.HIGH)
+GPIO.output(right_front_motor, GPIO.HIGH)
+GPIO.output(left_back_motor, GPIO.HIGH)
+GPIO.output(right_back_motor, GPIO.HIGH)
+sleep(2)
+
+#Stopping Motors
+GPIO.cleanup()
